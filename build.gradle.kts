b8da7de55d1955a971792aaa65aca72ef096ec91
Mark kevin cachuela <mark.cachuela@neoday.com>
2024-03-20 22:42:45 +0800

    Fix gradle

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
-}
-
-val composeVersion by extra("1.0.2")
